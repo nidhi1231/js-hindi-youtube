ed820c25a619f01c8a0cbfc4b96cd1479be4325c
Nidhi Dewangan <72557156+nidhi1231@users.noreply.github.com>
2025-10-14 11:05:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "Nidhi" //33/ "33" //"33abc"/ null / undefined / true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueNumber = Number(score)
+//console.log(typeof valueNumber);
+//console.log(valueNumber);
+
+let isLoggedin = ""
+let booleanIsLoggedIn = Boolean(isLoggedin)
+//console.log(booleanIsLoggedIn);
+ 
+// 1=> true , 0=> false
+// "" =? false
+//"Nidhi" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
