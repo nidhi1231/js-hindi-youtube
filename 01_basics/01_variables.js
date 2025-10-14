c4d98e5d1d3b5682880a71977cea697091062e99
Nidhi Dewangan <72557156+nidhi1231@users.noreply.github.com>
2025-10-14 11:03:38 +0000

    variable and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 14453  
+let accountEmail = "nidhi@gmail.com"
+var accountPasswword = "12345"
+accountCity = "Bhilai"   // posiblity to declare variable
+let accountState;
+
+
+//accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPasswword = "222111"
+accountCity = "Jaipur"
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPasswword,accountCity])
+console.log(accountState);
+
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3);  // wwe are using nodejs, not browser
+console.log(3
+    + 3) // code readability should be high
+
+console.log("Nidhi")
+let name = "Nidhi"
+let age = 18
+let isLoggedin = false
+// number => 2 to powwer 53
+// bigint  => facebook,flipcart jese
+// string = ""
+// boolean => true/false
+// null => standalone value (empty value ka represent)
+// undefined => value assign nhi hui h
+// symbol => unique
+
+// object
+
+console.log(typeof "Nidhi"); // type : String
+console.log(typeof age);    // type : number
+console.log(typeof null);   //type : object
+console.log(typeof undefined); // type : undefined
