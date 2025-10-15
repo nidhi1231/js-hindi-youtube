f74fd8df8d8e006aff34042514cd4d27c565c3dc
Nidhi Dewangan <72557156+nidhi1231@users.noreply.github.com>
2025-10-15 18:49:21 +0000

    string conversion

---
+++ 01_basics/04_stringConversion.js
@@ -0,0 +1,21 @@
+let str1 = "Hello"
+let str2 = " nidhi"
+let str3 = str1 +str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1+ "2");
+console.log("1" + 2+ 2);
+console.log(1 + 2+ "2");
+//console.log(3 + 4 *5 %3); //this is wrong way , not use for coding ,please use this paranthesis
+console.log((3 + 4) * 5 % 3);
+console.log(true);
+console.log(+true); 
+console.log(+ "");
+
+//let num1 ,num2 , num3
+//num1 = num2 = num3 = 2+2 // not use this type
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
