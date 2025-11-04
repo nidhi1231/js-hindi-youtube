c9d5f483d09831808127f6687c33bc50e8d035d9
Nidhi Dewangan <72557156+nidhi1231@users.noreply.github.com>
2025-11-04 12:30:22 +0530

    control flow statement in js

---
+++ 01_basics/one.js
@@ -0,0 +1,49 @@
+// if
+
+// const isUerLoggedIn = true
+// const temprature = 41
+// if(temprature === 40){
+//     console.log("less than 50");
+    
+// } else {
+//  console.log("temprature is grater than 50");
+// }
+
+
+// if( 2 != 3){
+// console.log("executed");
+
+// }
+
+//  <  less than, > greater than, <= less than, >= greater than , == two equal,!= not equal,=== triple equal
+// !== 
+
+const score = 200
+if(score > 100){
+    const power = "fly"
+    console.log(`user power: ${power}`);
+    
+} 
+//console.log(`user power: ${power}`); // power is not defined
+
+const balance = 10000
+//if(balance > 500) console.log("test");
+// pr apko aisa code nhi krna h
+// if(balance > 500) console.log("test1"),
+// console.log("test2"); this code not use
+
+// 
+
+const userLoggedIn = true
+const debitCard = true
+const loggedINFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2==3){
+    console.log("allow to buy courses");
+    
+}
+if(loggedINFromGoogle || loggedInFromEmail ){
+     console.log("User logged in");
+     
+}
---
+++ 01_basics/switch.js
@@ -0,0 +1,48 @@
+// syntax of switch case
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April"); 
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+const month = "March"
+switch (month) {
+    case "Jan":
+        console.log("Jan");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April"); 
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
